8e71642718db6d8c84699d9769504fe443bbc542
Randy Greig <randy@greigfamily.ca>
2016-02-08 11:25:58 -0700

    20160208 updated rlgNode with new base code from lowpowerlabs

--- rlgNode/rlgNode.ino
+++ rlgNode/rlgNode.ino
@@ -1,36 +1,24 @@
 // Sample RFM69 sender/node sketch, with ACK and optional encryption, and Automatic Transmission Control
 // Sends periodic messages of increasing length to gateway (id=1)
-// It also looks for an onboard FLASH chip, if present
 // RFM69 library and sample code by Felix Rusu - http://LowPowerLab.com/contact
 // Copyright Felix Rusu (2015)
 
 #include <RFM69.h>    //get it here: https://www.github.com/lowpowerlab/rfm69
 #include <RFM69_ATC.h>//get it here: https://www.github.com/lowpowerlab/rfm69
 #include <SPI.h>
-#include <SPIFlash.h> //get it here: https://www.github.com/lowpowerlab/spiflash
 
 //*********************************************************************************************
 //************ IMPORTANT SETTINGS - YOU MUST CHANGE/CONFIGURE TO FIT YOUR HARDWARE *************
 //*********************************************************************************************
 #define NODEID        2    //must be unique for each node on same network (range up to 254, 255 is used for broadcast)
-#define NETWORKID     100  //the same on all nodes that talk to each other (range up to 255)
+#define NETWORKID     123  //the same on all nodes that talk to each other (range up to 255)
 #define GATEWAYID     1
-//Match frequency to the hardware version of the radio on your Moteino (uncomment one):
-#define FREQUENCY   RF69_433MHZ
-//#define FREQUENCY   RF69_868MHZ
-//#define FREQUENCY     RF69_915MHZ
-#define ENCRYPTKEY    "sampleEncryptKey" //exactly the same 16 characters/bytes on all nodes!
-//#define IS_RFM69HW    //uncomment only for RFM69HW! Leave out if you have RFM69W!
+#define FREQUENCY     RF69_915MHZ
+#define ENCRYPTKEY    "GFmoteEncryptKey" //exactly the same 16 characters/bytes on all nodes!
 #define ENABLE_ATC    //comment out this line to disable AUTO TRANSMISSION CONTROL
 //*********************************************************************************************
 
-#ifdef __AVR_ATmega1284P__
-  #define LED           15 // Moteino MEGAs have LEDs on D15
-  #define FLASH_SS      23 // and FLASH SS on D23
-#else
-  #define LED           9 // Moteinos have LEDs on D9
-  #define FLASH_SS      8 // and FLASH SS on D8
-#endif
+#define LED           9 // Moteinos have LEDs on D9
 
 #define SERIAL_BAUD   115200
 
@@ -39,20 +27,12 @@ char payload[] = "123 ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char buff[20];
 byte sendSize=0;
 boolean requestACK = false;
-SPIFlash flash(FLASH_SS, 0xEF30); //EF30 for 4mbit  Windbond chip (W25X40CL)
 
-#ifdef ENABLE_ATC
-  RFM69_ATC radio;
-#else
-  RFM69 radio;
-#endif
+RFM69_ATC radio;
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
   radio.initialize(FREQUENCY,NODEID,NETWORKID);
-#ifdef IS_RFM69HW
-  radio.setHighPower(); //uncomment only for RFM69HW!
-#endif
   radio.encrypt(ENCRYPTKEY);
   //radio.setFrequency(919000000); //set frequency to some custom frequency
   
@@ -60,31 +40,13 @@ void setup() {
 //For indoor nodes that are pretty static and at pretty stable temperatures (like a MotionMote) -90dBm is quite safe
 //For more variable nodes that can expect to move or experience larger temp drifts a lower margin like -70 to -80 would probably be better
 //Always test your ATC mote in the edge cases in your own environment to ensure ATC will perform as you expect
-#ifdef ENABLE_ATC
   radio.enableAutoPower(-70);
-#endif
   
   char buff[50];
   sprintf(buff, "\nTransmitting at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
   Serial.println(buff);
   
-  if (flash.initialize())
-  {
-    Serial.print("SPI Flash Init OK ... UniqueID (MAC): ");
-    flash.readUniqueId();
-    for (byte i=0;i<8;i++)
-    {
-      Serial.print(flash.UNIQUEID[i], HEX);
-      Serial.print(' ');
-    }
-    Serial.println();
-  }
-  else
-    Serial.println("SPI Flash MEM not found (is chip soldered?)...");
-    
-#ifdef ENABLE_ATC
   Serial.println("RFM69_ATC Enabled (Auto Transmission Control)\n");
-#endif
 }
 
 long lastPeriod = 0;
@@ -104,37 +66,6 @@ void loop() {
 
     if (input == 'r') //d=dump register values
       radio.readAllRegs();
-    //if (input == 'E') //E=enable encryption
-    //  radio.encrypt(KEY);
-    //if (input == 'e') //e=disable encryption
-    //  radio.encrypt(null);
-
-    if (input == 'd') //d=dump flash area
-    {
-      Serial.println("Flash content:");
-      uint16_t counter = 0;
-
-      Serial.print("0-256: ");
-      while(counter<=256){
-        Serial.print(flash.readByte(counter++), HEX);
-        Serial.print('.');
-      }
-      while(flash.busy());
-      Serial.println();
-    }
-    if (input == 'e')
-    {
-      Serial.print("Erasing Flash chip ... ");
-      flash.chipErase();
-      while(flash.busy());
-      Serial.println("DONE");
-    }
-    if (input == 'i')
-    {
-      Serial.print("DeviceID: ");
-      word jedecid = flash.readDeviceId();
-      Serial.println(jedecid, HEX);
-    }
   }
 
   //check for any received packets
@@ -159,10 +90,9 @@ void loop() {
   {
     lastPeriod=currPeriod;
     
-    //send FLASH id
     if(sendSize==0)
     {
-      sprintf(buff, "FLASH_MEM_ID:0x%X", flash.readDeviceId());
+      sprintf(buff, "FLASH_MEM_ID:0x%X");
       byte buffLen=strlen(buff);
       if (radio.sendWithRetry(GATEWAYID, buff, buffLen))
         Serial.print(" ok!");
